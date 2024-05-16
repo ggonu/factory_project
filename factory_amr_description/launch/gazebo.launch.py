f7087290a2bb3c70262666f79f4f8118871ef33a
ggonu <gogun0928@naver.com>
2024-05-16 16:39:17 +0900

    upload files

--- factory_amr_description/launch/gazebo.launch.py
+++ factory_amr_description/launch/gazebo.launch.py
@@ -24,6 +24,12 @@ def generate_launch_description():
             {'robot_description': robot_urdf}
         ]
     )
+    
+    rsp = IncludeLaunchDescription(
+        PythonLaunchDescriptionSource([os.path.join(
+            get_package_share_directory('factory_amr_description'), 'launch', 'rsp.launch.py'
+        )]), launch_arguments={'use_sim_time': 'true'}.items()
+    )
 
     joint_state_publisher_node = Node(
         package='joint_state_publisher',
@@ -65,7 +71,8 @@ def generate_launch_description():
     )
 
     return LaunchDescription([
-        robot_state_publisher_node,
+        # robot_state_publisher_node,
+        rsp,
         joint_state_publisher_node,
         gazebo_server,
         gazebo_client,
